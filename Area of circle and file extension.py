765fc7840052289a7e3e5f175fefa693197cae67
Bhargavi kotha <68498062+kothabhargavi@users.noreply.github.com>
2020-09-11 18:42:51 +0530

    To find the area of circle and the file extension

---
+++ Area of circle and file extension.py
@@ -0,0 +1,11 @@
+#first program
+a=float (input('Input the radius of the circle:'))
+r=1.1
+Print('The area of the circle with radius 1.1 is:',3.14*r*r)
+
+
+
+#second program
+filename = input("Input the Filename: ")
+f_extns = filename.split(".")
+print ("The extension of the file is : " + repr(f_extns[-1]))
